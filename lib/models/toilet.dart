29c15cd1c5af1f44a018fe5f9f7c8c533b87f230
✿ -mameaw- ✿ <chutinunpoolsup@mm.local>
2023-10-01 14:26:41 +0700

    Initial commit

---
+++ lib/models/toilet.dart
@@ -0,0 +1,19 @@
+class Toilet {
+  final String name;
+  final double point;
+  final double distance;
+
+  Toilet({
+    required this.name,
+    required this.point,
+    required this.distance,
+  });
+
+  factory Toilet.fromJson(Map<String, dynamic> json) {
+    return Toilet(
+      name: json["name"],
+      point: json["rating"],
+      distance: json["distance"],
+    );
+  }
+}
